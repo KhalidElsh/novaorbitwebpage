134830f82f5128291754d6d3aafdfe07b40a9967
Elsh22 <76716965+Elsh22@users.noreply.github.com>
2025-01-12 22:00:08 -0500

    u

--- components/VideoBackground.tsx
+++ components/VideoBackground.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from 'react';
 import dynamic from 'next/dynamic';
+import video from '../assets/background.mp4'
 
 const VideoBackground = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -25,7 +26,7 @@ const VideoBackground = () => {
         playsInline
         className="w-full h-full object-cover"
       >
-        <source src="/background.mp4" type="video/mp4" />
+        <source src={video} type="video/mp4" />
       </video>
       <div className="absolute inset-0 bg-black/50" />
     </div>
